3ecebeb166491f6a9ad46f513ea3d097bd966459
smskbuilds <smskbuilds@gmail.com>
2024-10-14 10:54:19 -0700

    Updated from default Vite project files to Tenzies code so far.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import Die from './Die';
+import { nanoid } from 'nanoid';
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+    /**
+     * Challenge: Create a function `holdDice` that takes
+     * `id` as a parameter. For now, just have the function
+     * console.log(id).
+     *
+     * Then, figure out how to pass that function down to each
+     * instance of the Die component so when each one is clicked,
+     * it logs its own unique ID property. (Hint: there's more
+     * than one way to make that work, so just choose whichever
+     * you want)
+     *
+     */
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const [dice, setDice] = React.useState(allNewDice());
+
+    function allNewDice() {
+        const newDice = [];
+        for (let i = 0; i < 10; i++) {
+            newDice.push({
+                value: Math.ceil(Math.random() * 6),
+                isHeld: false,
+                id: nanoid(),
+            });
+        }
+        return newDice;
+    }
+
+    function rollDice() {
+        setDice(allNewDice());
+    }
+
+    const diceElements = dice.map((die) => (
+        <Die key={die.id} value={die.value} isHeld={die.isHeld} />
+    ));
+
+    return (
+        <main>
+            <div className='dice-container'>{diceElements}</div>
+            <button className='roll-dice' onClick={rollDice}>
+                Roll
+            </button>
+        </main>
+    );
 }
-
-export default App
